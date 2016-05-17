e9f1686edf72900c42d8c698b4d15de86a96912a
apeloza <apeloza@alumni.uwo.ca>
2016-05-16 23:43:50 -0500

    Adds sound and text effect

--- server/public/scripts/app.js
+++ server/public/scripts/app.js
@@ -1,12 +1,18 @@
 var textIndex = 0;
 var linebyLine;
 var parsedText = [];
+var blipMale = new Audio ('../assets/audio/sfx/sfx-blipmale.wav');
+var speechDelay;
+var substringIndex = 0;
 $(document).ready(function () {
   $('.switchanim').on('click', function () {
     $('.character').toggleClass('gumshoetalk');
   });
   $('.textbox').on('click', advanceText);
 fetchText();
+var courtroomLobby = new Audio('../assets/audio/bgm/courtroomlobby.mp3');
+courtroomLobby.loop = true;
+courtroomLobby.play();
 
 });
 
@@ -14,14 +20,12 @@ function fetchText(){
   $.get('/assets/text/dummytext.txt', function(data){
     console.log(data);
     linebyLine = data.split("\n");
-    console.log(linebyLine[1]);
 parseText(linebyLine);
   });
 }
 function parseText(text){
   for (var i = 0; i < linebyLine.length; i++){
     parsedText.push(linebyLine[i].split('::'));
-    console.log(parsedText);
   }
 }
 
@@ -29,10 +33,23 @@ function advanceText(){
   if (linebyLine.length <= textIndex + 1 ){
     return true;
   } else {
-$('.textbox').text(parsedText[textIndex][1]);
-$('.portrait').attr('src', '../assets/sprites/' + parsedText[textIndex][0] + '.gif')
-console.log(parsedText.length);
-console.log(textIndex);
-textIndex++;
+    $('.textbox').empty();
+    speechDelay = setInterval (typeText, 60);
+$('.portrait').attr('src', '../assets/sprites/' + parsedText[textIndex][0] + '.gif');
 }
+}
+
+function typeText(){
+  console.log(parsedText[textIndex][1]);
+  $('.textbox').append(parsedText[textIndex][1][substringIndex]);
+  substringIndex++;
+  blipMale.play();
+  if(substringIndex > parsedText[textIndex][1].length - 1){
+    console.log(substringIndex);
+    console.log(parsedText[textIndex][1].length);
+    console.log("True");
+    clearInterval(speechDelay);
+    substringIndex = 0;
+    textIndex++;
+  }
 }
