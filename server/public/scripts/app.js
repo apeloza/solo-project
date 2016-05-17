405bb9e1612b30dd1c8ce4b9829eea23bedd14f8
apeloza <apeloza@alumni.uwo.ca>
2016-05-16 22:57:03 -0500

    Synced up animations with text

--- server/public/scripts/app.js
+++ server/public/scripts/app.js
@@ -1,5 +1,6 @@
 var textIndex = 0;
 var linebyLine;
+var parsedText = [];
 $(document).ready(function () {
   $('.switchanim').on('click', function () {
     $('.character').toggleClass('gumshoetalk');
@@ -14,11 +15,24 @@ function fetchText(){
     console.log(data);
     linebyLine = data.split("\n");
     console.log(linebyLine[1]);
-
+parseText(linebyLine);
   });
+}
+function parseText(text){
+  for (var i = 0; i < linebyLine.length; i++){
+    parsedText.push(linebyLine[i].split('::'));
+    console.log(parsedText);
+  }
 }
 
 function advanceText(){
-$('.textbox').text(linebyLine[textIndex]);
+  if (linebyLine.length <= textIndex + 1 ){
+    return true;
+  } else {
+$('.textbox').text(parsedText[textIndex][1]);
+$('.portrait').attr('src', '../assets/sprites/' + parsedText[textIndex][0] + '.gif')
+console.log(parsedText.length);
+console.log(textIndex);
 textIndex++;
+}
 }
