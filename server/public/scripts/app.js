ac070bb0fd978a5e957f24526ccebdd3376164b3
apeloza <apeloza@alumni.uwo.ca>
2016-05-17 21:25:25 -0500

    Improved text

--- server/public/scripts/app.js
+++ server/public/scripts/app.js
@@ -1,31 +1,38 @@
-var textIndex = 0;
+
+//Many variables are initialized here to be manipulated globally.
+var textIndex = 1;
 var linebyLine;
 var parsedText = [];
-var blipMale = new Audio ('../assets/audio/sfx/sfx-blipmale.wav');
+var blip;
 var speechDelay;
 var substringIndex = 0;
+
 $(document).ready(function () {
-  $('.switchanim').on('click', function () {
-    $('.character').toggleClass('gumshoetalk');
-  });
+
+//When the textbox is clicked on, the text is advanced to the next line.
   $('.textbox').on('click', advanceText);
+
+//fetchText takes in data from a .txt file and parses it.
 fetchText();
+
+//The starting music is initialized. It is set to loop, and then it plays.
 var courtroomLobby = new Audio('../assets/audio/bgm/courtroomlobby.mp3');
 courtroomLobby.loop = true;
 courtroomLobby.play();
 
 });
 
+//This function takes in a .txt file with an ajax request. It splits it into individual lines, which are then fed to a text parser.
 function fetchText(){
   $.get('/assets/text/dummytext.txt', function(data){
-    console.log(data);
     linebyLine = data.split("\n");
 parseText(linebyLine);
   });
 }
 function parseText(text){
-  for (var i = 0; i < linebyLine.length; i++){
-    parsedText.push(linebyLine[i].split('::'));
+
+  for (var i = 0; i < text.length; i++){
+    parsedText.push(text[i].split('::'));
   }
 }
 
@@ -35,22 +42,22 @@ function advanceText(){
     return true;
   } else {
     $('.textbox').empty();
+    $('.textbox').removeClass(parsedText[textIndex - 1][4]);
     $('.textbox').off('click', advanceText);
+    blip = new Audio ('../assets/audio/sfx/sfx-' + parsedText[textIndex][3] + '.wav');
+    $('.textbox').addClass(parsedText[textIndex][4]);
     speechDelay = setInterval (typeText, 50);
 $('.portrait').attr('src', '../assets/sprites/' + parsedText[textIndex][0] + '.gif');
 }
 }
 
 function typeText(){
-
   console.log(parsedText[textIndex][1]);
   $('.textbox').append(parsedText[textIndex][1][substringIndex]);
   substringIndex++;
-  blipMale.play();
+  blip.play();
   if(substringIndex > parsedText[textIndex][1].length - 1){
-    console.log(substringIndex);
-    console.log(parsedText[textIndex][1].length);
-    console.log("True");
+
     clearInterval(speechDelay);
     substringIndex = 0;
     $('.portrait').attr('src', '../assets/sprites/' + parsedText[textIndex][2] + '.gif');
