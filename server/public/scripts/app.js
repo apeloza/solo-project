545c913c1a8954ac14f313c65da758af4c3a385e
apeloza <apeloza@alumni.uwo.ca>
2016-05-22 17:52:27 -0500

    Evidence button working

--- server/public/scripts/app.js
+++ server/public/scripts/app.js
@@ -1,10 +1,10 @@
-
 //Many variables are initialized here to be manipulated globally.
 var isTalking = false;
 var speechDelay;
 var substringIndex = 0;
 var characterList;
 var sceneList;
+var evidenceList;
 var currChar;
 var currScene;
 var ruby;
@@ -13,164 +13,181 @@ var opening;
 
 //This is the character prototype.
 var Character = function(params) {
-  this.name = params.name;
-  this.emotions = params.emotions;
-  this.emotion = 'default';
-  this.sound = params.defaultSound;
-  this.text = params.defaultText;
+    this.name = params.name;
+    this.emotions = params.emotions;
+    this.emotion = 'default';
+    this.sound = params.defaultSound;
+    this.text = params.defaultText;
 };
 
 var Scene = function(params) {
-  this.lines = params.lines;
-  this.background = params.background;
-  this.character = params.character;
-  this.lineIndex = 0;
+    this.lines = params.lines;
+    this.background = params.background;
+    this.character = params.character;
+    this.lineIndex = 0;
+};
+
+var Evidence = function(params) {
+    this.name = params.name;
+    this.description = params.description;
+    this.image = params.image;
+    this.info = params.info;
+
 };
 
 
-
 //This function outputs text onto the DOM and handles gifs/sound effects.
-Character.prototype.speak = function(message, emotion, speechtype){
-  if (isTalking === false){
-    isTalking = true;
-  this.emotion = emotion || 'default';
-  var $textbox = $('.textbox');
-      $textbox.empty();
-      $textbox.attr('class', 'textbox ' + (speechtype || this.text));
-      $('.namebox').text(this.name);
-      blip = new Audio ('../assets/audio/sfx/sfx-' + this.sound + '.wav');
-      console.log(this.emotions);
-      displaySprite(this.emotions[this.emotion].talking);
-      this.showText(message, 0, 50, blip);
+Character.prototype.speak = function(message, emotion, speechtype) {
+    if (isTalking === false) {
+        isTalking = true;
+        this.emotion = emotion || 'default';
+        var $textbox = $('.textbox');
+        $textbox.empty();
+        $textbox.attr('class', 'textbox ' + (speechtype || this.text));
+        $('.namebox').text(this.name);
+        blip = new Audio('../assets/audio/sfx/sfx-' + this.sound + '.wav');
+        console.log(this.emotions);
+        displaySprite(this.emotions[this.emotion].talking);
+        this.showText(message, 0, 50, blip);
     }
 };
 
 //This function is in charge of appending to the DOM one letter at a time.
-Character.prototype.showText = function (message, index, interval, sound) {
-  if (index < message.length) {
+Character.prototype.showText = function(message, index, interval, sound) {
+    if (index < message.length) {
 
-    $('.textbox').append(message[index++]);
-    sound.play();
-    var self = this;
-    setTimeout(function () { self.showText(message, index, interval, sound); }, interval);
-  } else {
-    currScene.lineIndex ++;
-    displaySprite(this.emotions[this.emotion].finished);
-    isTalking = false;
-  }
+        $('.textbox').append(message[index++]);
+        sound.play();
+        var self = this;
+        setTimeout(function() {
+            self.showText(message, index, interval, sound);
+        }, interval);
+    } else {
+        currScene.lineIndex++;
+        displaySprite(this.emotions[this.emotion].finished);
+        isTalking = false;
+    }
 };
-Scene.prototype.advanceText = function(){
-  var line = currScene.lines[currScene.lineIndex];
-  var emotion = currScene.lines[currScene.lineIndex].emotion;
-  var texttype = currScene.lines[currScene.lineIndex].text;
-  console.log(characterList[currScene.lines[currScene.lineIndex].character]);
-currChar = (characterList[currScene.lines[currScene.lineIndex].character]) || currChar;
-console.log(currChar);
-currChar.speak(line.line || currScene.lines[currScene.lineIndex], emotion, texttype);
-currScene.changeBG(line.background || currScene.background);
-if(currScene.lines.length <= currScene.lineIndex + 1){
-  //this.nextScene();
-}
+Scene.prototype.advanceText = function() {
+    var line = currScene.lines[currScene.lineIndex];
+    var emotion = currScene.lines[currScene.lineIndex].emotion;
+    var texttype = currScene.lines[currScene.lineIndex].text;
+    console.log(characterList[currScene.lines[currScene.lineIndex].character]);
+    currChar = (characterList[currScene.lines[currScene.lineIndex].character]) || currChar;
+    console.log(currChar);
+    currChar.speak(line.line || currScene.lines[currScene.lineIndex], emotion, texttype);
+    currScene.changeBG(line.background || currScene.background);
+    if (currScene.lines.length <= currScene.lineIndex + 1) {
+        //this.nextScene();
+    }
 };
-Scene.prototype.changeBG = function(bg){
-  console.log("Fired changeBG");
-$('.gamecontainer').css({'backgroundImage' : 'url(../assets/backgrounds/' + bg + '.png'});
+Scene.prototype.changeBG = function(bg) {
+    $('.gamecontainer').css({
+        'backgroundImage': 'url(../assets/backgrounds/' + bg + '.png'
+    });
 
 };
 
-$(document).ready(function () {
+$(document).ready(function() {
 
 
-fetchChar('isaac');
-fetchScene('opening');
+    fetchChar();
+    fetchScene();
+    fetchEvidence();
 
-//The starting music is initialized. It is set to loop, and then it plays.
-var courtroomLobby = new Audio('../assets/audio/bgm/courtroomlobby.mp3');
-courtroomLobby.loop = true;
-courtroomLobby.play();
+    //The starting music is initialized. It is set to loop, and then it plays.
+    var courtroomLobby = new Audio('../assets/audio/bgm/courtroomlobby.mp3');
+    courtroomLobby.loop = true;
+    courtroomLobby.play();
 
 });
 
 //This function fetches all of our characters in a json file.
-function fetchChar(){
-  $.ajax({
-      type: 'get',
-      url: '/jsonserver/characters/',
-      success: function(character) {
-console.log(character);
-        characterList = character;
-        ruby = new Character(characterList.ruby);
-        isaac = new Character(characterList.isaac);
-        characterList = {
-          ruby: ruby,
-          isaac: isaac
-        };
-        checkLoad();
-      }
-  });
+function fetchChar() {
+    $.ajax({
+        type: 'get',
+        url: '/jsonserver/characters/',
+        success: function(character) {
+            characterList = character;
+
+            //New characters are made for each character in the json file. They are then put into a container object.
+            ruby = new Character(characterList.ruby);
+            isaac = new Character(characterList.isaac);
+            characterList = {
+                ruby: ruby,
+                isaac: isaac
+            };
+            checkLoad();
+        }
+    });
 }
 
 //This function fetches all of our scenes in a json file.
-function fetchScene(){
-  $.get('/jsonserver/scene/', function(linedata){
-    console.log(linedata);
-sceneList = linedata;
-opening = new Scene(sceneList.opening);
-sceneList = {
-  opening: opening
-};
-checkLoad();
-  });
+function fetchScene() {
+    $.get('/jsonserver/scene/', function(linedata) {
+        sceneList = linedata;
+        opening = new Scene(sceneList.opening);
+        sceneList = {
+            opening: opening
+        };
+        checkLoad();
+    });
 }
 
+function fetchEvidence() {
+    $.get('/jsonserver/evidence/', function(evidence) {
+        console.log(evidence);
+        badge = new Evidence(evidence.badge);
+        autopsyreport = new Evidence(evidence.autopsyreport);
+        evidenceList = {
+            badge: badge,
+            autopsyreport: autopsyreport
+        };
+        checkLoad();
+    });
+}
 //This function alters the sprite displayed to a new URL.
-function displaySprite(path){
-$('.portrait').attr('src', '../assets/sprites/' + path + '.gif');
+function displaySprite(path) {
+    $('.portrait').attr('src', '../assets/sprites/' + path + '.gif');
 
+}
+
+function displayEvidence() {
+    $('.evidencebox').removeClass('hidden');
+    $('.portrait').addClass('hidden');
+}
+
+function hideEvidence() {
+    $('.evidencebox').addClass('hidden');
+    $('.portrait').removeClass('hidden');
+}
+
+function setActiveEvidence() {
+  $('.evidence').removeClass('active');
+    $(this).addClass('active');
+    active = $(this).attr('id');
+    console.log(active);
+    $('.textbox').text(evidenceList[active].description);
+}
+
+function showInfo() {
+    if (active) {
+        $('.textbox').text(evidenceList[active].info);
+    }
 }
 
 //This function ensures all our JSON files are ready before the game begins.
-function checkLoad(){
-  if (characterList && sceneList){
-    console.log("Yep");
+function checkLoad() {
+    if (characterList && sceneList && evidenceList) {
+        console.log("Yep");
 
-     currScene = (sceneList.opening);
-     currChar = (characterList[currScene.character]);
-     console.log(currScene.character);
-     console.log(currChar);
-    $('.textbox').on('click', currScene.advanceText);
-  }
+        currScene = (sceneList.opening);
+        currChar = (characterList[currScene.character]);
+
+        $('.textbox').on('click', currScene.advanceText);
+        $('.evidencebtn').on('click', displayEvidence);
+        $('.close').on('click', hideEvidence);
+        $('.evidence').on('click', setActiveEvidence);
+        $('.check').on('click', showInfo);
+    }
 }
-
-
-/*function advanceText(message, options){
-var $textbox = $('.textbox');
-    $textbox.empty();
-    $textbox.attr('class', 'textbox ' + options.textType);
-    blip = new Audio ('../assets/audio/sfx/sfx-' + options.textSound + '.wav');
-    showText(message, 0, 50, blip);
-}
-
-function typeText(){
-  console.log(parsedText[textIndex][1]);
-  $('.textbox').append(parsedText[textIndex][1][substringIndex]);
-  substringIndex++;
-  blip.play();
-  if(substringIndex > parsedText[textIndex][1].length - 1){
-
-    clearInterval(speechDelay);
-    substringIndex = 0;
-    $('.textbox').append('<img class="pointer" src="../assets/interfaceimages/pointer.gif" />');
-    textIndex++;
-    $('.textbox').on('click', advanceText);
-
-  }
-}
-
-function showText (message, index, interval, sound) {
-  if (index < message.length) {
-    $('.textbox').append(message[index++]);
-    sound.play();
-    setTimeout(function () { showText(message, index, interval, sound); }, interval);
-  }
-}*/
--- server/routes/jsonserver.js
+++ server/routes/jsonserver.js
@@ -2,12 +2,16 @@ var express = require('express');
 var characters = require('../public/assets/text/characters');
 var router = express.Router();
 var scenes = require('../public/assets/text/scenes');
-
+var evidence = require('../public/assets/text/evidence');
 router.get('/characters/', function(req, res) {
 res.send(characters);
 });
 
 router.get('/scene/', function(req, res) {
 res.send(scenes);
+});
+
+router.get('/evidence/', function(req, res) {
+  res.send(evidence);
 });
 module.exports = router;
